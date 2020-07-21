040a1f75aecfc3f97419f64103024fd5fb8c827e
Patrick Maier <mail@maierp.de>
2020-07-21 17:28:27 +0200

    1

--- Debug.cpp
+++ Debug.cpp
@@ -1,5 +1,4 @@
 #include <sys/time.h>
-#include <iostream>
 #include <cstdarg>
 
 #include "Debug.h"
@@ -10,8 +9,6 @@ void LOGCONSOLE(int x, int y, const char* msg, ...) {
     struct timeval now;
     gettimeofday(&now, NULL);
     struct tm* tm = gmtime(&now.tv_sec);
-    char buffer[30];
-    sprintf(buffer, "%04d-%02d-%02d %02d:%02d:%02d.%03lu ", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec, now.tv_usec / 1000U);
-    std::cout << buffer;
+    printf("%04d-%02d-%02d %02d:%02d:%02d.%03lu ", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec, now.tv_usec / 1000U);
     vprintf(msg, args);
 }
